71641256c2b3724df31eaf783a82e83c61f2036d
diatsilva007 <diatsilva007@gmail.com>
2024-12-25 20:32:13 -0300

    feat: código js para transformar menu hamburguer em x adicionado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-// Adicione aqui script.js
+/* Script.js here... */
 
-// alert("Teste")
+/* Transformar menu hamburguer em um x */
+const menuHamburguer = document.querySelector(".menu-hamburguer");
+menuHamburguer.addEventListener("click", () => {
+  toggleMenu();
+});
+
+function toggleMenu() {
+  const nav = document.querySelector(".nav-responsive");
+  menuHamburguer.classList.toggle("change");
+
+  if (menuHamburguer.classList.contains("change")) {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
+}
