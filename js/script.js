96d188cc4a7c9114f2a19624c76102bd2baa62f1
diatsilva007 <diatsilva007@gmail.com>
2024-12-25 21:04:30 -0300

    feat: código js para tema dark e light

--- js/script.js
+++ js/script.js
@@ -16,3 +16,25 @@ function toggleMenu() {
     nav.style.display = "none";
   }
 }
+
+/* Verifica se o usuário já tem uma preferência de tema salva*/
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    document.body.classList.add(savedTheme);
+}
+
+/* Seleciona o botão para alternar o tema */
+const toggleButton = document.getElementById('toggle-theme');
+
+/* Adiciona um ouvinte de evento para trocar os temas ao clicar no botão */
+toggleButton.addEventListener('click', () => {
+    /* Alterna a classe "dark-mode" no body */
+    document.body.classList.toggle('dark-mode');
+
+    /* Salva a preferência do tema no localStorage */
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.setItem('theme', '');
+    }
+});
