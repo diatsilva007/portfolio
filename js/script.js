0ec56a9fb8781a01f09a5024b4b5bd01e02cae97
diatsilva007 <diatsilva007@gmail.com>
2025-01-01 22:42:14 -0300

    feat: Adicionado código js para efeito typing com apagar

--- js/script.js
+++ js/script.js
@@ -19,15 +19,47 @@ function toggleMenu() {
 
 /* Código para telefone de formulário */
 function mascaraTelefone(input) {
-    // Remove tudo o que não for número
-    let telefone = input.value.replace(/\D/g, '');
+  /* Remove tudo o que não for número */
+  let telefone = input.value.replace(/\D/g, "");
 
-    // Formatação (XX) XXXXX-XXXX
-    if (telefone.length <= 10) {
-        telefone = telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3'); // Formato (XX) XXXXX-XXXX
-    } else {
-        telefone = telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3'); // Mantém o formato
-    }
+  /* Formatação (XX) XXXXX-XXXX */
+  if (telefone.length <= 10) {
+    telefone = telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3"); // Formato (XX) XXXXX-XXXX
+  } else {
+    telefone = telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3"); // Mantém o formato
+  }
 
-    input.value = telefone;
+  input.value = telefone;
 }
+
+/* Efeito typing com apagar */
+const texts = ["Front-End Developer", "Full-Stack Developer", "Jr. Software Engineer",];
+const typingEffect = document.getElementById("typing-effect");
+
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentText = texts[textIndex];
+  const speed = isDeleting ? 50 : 100; // Velocidade de digitação e apagamento
+
+  if (isDeleting) {
+    typingEffect.textContent = currentText.substring(0, charIndex--);
+  } else {
+    typingEffect.textContent = currentText.substring(0, charIndex++);
+  }
+
+  if (!isDeleting && charIndex === currentText.length) {
+    isDeleting = true;
+    setTimeout(type, 1000); // Pausa antes de apagar
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    textIndex = (textIndex + 1) % texts.length; // Avança para o próximo texto
+    setTimeout(type, 500); // Pausa antes de começar a digitar o próximo
+  } else {
+    setTimeout(type, speed);
+  }
+}
+
+type();
