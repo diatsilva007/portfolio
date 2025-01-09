bc364474f5b86a997ac6051fcc31ba9160d138e5
diatsilva007 <diatsilva007@gmail.com>
2025-01-08 22:25:22 -0300

    feat: Script para alternar visibilidade do conteúdo "Leia Mais"

--- js/script.js
+++ js/script.js
@@ -63,3 +63,15 @@ function type() {
 }
 
 type();
+
+ // Script para alternar visibilidade do conteúdo "Leia Mais"
+ document.getElementById('leiaMaisBtn').addEventListener('click', function () {
+  var content = document.getElementById('leiaMais');
+  if (content.classList.contains('hidden-content')) {
+      content.classList.remove('hidden-content');
+      content.classList.add('visible-content');
+  } else {
+      content.classList.remove('visible-content');
+      content.classList.add('hidden-content');
+  }
+});
