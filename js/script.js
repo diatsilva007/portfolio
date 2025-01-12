019718e13bcc14775c2d4c3b1c9b642c204c7148
diatsilva007 <diatsilva007@gmail.com>
2025-01-12 00:20:36 -0300

    feat: Código js para efeito partículas

--- js/script.js
+++ js/script.js
@@ -2,13 +2,87 @@
 
 // JavaScript (para adicionar a classe quando o usuário rolar para baixo e aparecer rodapé)
 window.onscroll = function () {
-  const footer = document.querySelector('footer');
-  if (window.scrollY > 100) {  // Muda a classe após o scroll de 100px
-      document.body.classList.add('scrolled');
+  const footer = document.querySelector("footer");
+  if (window.scrollY > 100) {
+    // Muda a classe após o scroll de 100px
+    document.body.classList.add("scrolled");
   } else {
-      document.body.classList.remove('scrolled');
+    document.body.classList.remove("scrolled");
   }
 };
+
+// Efeito particle em js
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 55,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+    },
+  },
+  retina_detect: true,
+});
 
 /* Transformar menu hamburguer em um x */
 const menuHamburguer = document.querySelector(".menu-hamburguer");
@@ -43,7 +117,11 @@ function mascaraTelefone(input) {
 }
 
 /* Efeito typing com apagar */
-const texts = ["Front-End Developer", "Full-Stack Developer", "Jr. Software Engineer",];
+const texts = [
+  "Front-End Developer",
+  "Full-Stack Developer",
+  "Jr. Software Engineer",
+];
 const typingEffect = document.getElementById("typing-effect");
 
 let textIndex = 0;
@@ -74,14 +152,14 @@ function type() {
 
 type();
 
- // Script para alternar visibilidade do conteúdo "Leia Mais"
- document.getElementById('leiaMaisBtn').addEventListener('click', function () {
-  var content = document.getElementById('leiaMais');
-  if (content.classList.contains('hidden-content')) {
-      content.classList.remove('hidden-content');
-      content.classList.add('visible-content');
+// Script para alternar visibilidade do conteúdo "Leia Mais"
+document.getElementById("leiaMaisBtn").addEventListener("click", function () {
+  var content = document.getElementById("leiaMais");
+  if (content.classList.contains("hidden-content")) {
+    content.classList.remove("hidden-content");
+    content.classList.add("visible-content");
   } else {
-      content.classList.remove('visible-content');
-      content.classList.add('hidden-content');
+    content.classList.remove("visible-content");
+    content.classList.add("hidden-content");
   }
 });
