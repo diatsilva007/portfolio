1fe525956f571cfe8d1c9fd1addf86350147a198
diatsilva007 <diatsilva007@gmail.com>
2025-01-20 23:07:44 -0300

    feat: Refatoração do script "Leia Mais" e adição de novo arquivo para desenvolvimento móvel

--- js/my_services_web_development.js
+++ js/my_services_web_development.js
@@ -1,21 +1,22 @@
 // Script para alternar visibilidade do conteúdo "Leia Mais"
-document.getElementById('leiaMaisBtn').addEventListener('click', function () {
-    var content = document.getElementById('leiaMais');
-    if (content.classList.contains('hidden-content')) {
-        content.classList.remove('hidden-content');
-        content.classList.add('visible-content');
-    } else {
-        content.classList.remove('visible-content');
-        content.classList.add('hidden-content');
-    }
+document.getElementById("leiaMaisBtn").addEventListener("click", function () {
+  var content = document.getElementById("leiaMais");
+  if (content.classList.contains("hidden-content")) {
+    content.classList.remove("hidden-content");
+    content.classList.add("visible-content");
+  } else {
+    content.classList.remove("visible-content");
+    content.classList.add("hidden-content");
+  }
 });
 
 // JavaScript (para adicionar a classe quando o usuário rolar para baixo)
 window.onscroll = function () {
-    const footer = document.querySelector('footer');
-    if (window.scrollY > 100) {  // Muda a classe após o scroll de 100px
-        document.body.classList.add('scrolled');
-    } else {
-        document.body.classList.remove('scrolled');
-    }
+  const footer = document.querySelector("footer");
+  if (window.scrollY > 100) {
+    // Muda a classe após o scroll de 100px
+    document.body.classList.add("scrolled");
+  } else {
+    document.body.classList.remove("scrolled");
+  }
 };
